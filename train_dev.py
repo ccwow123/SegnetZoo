c4f543e82d67a47bfabfa8b0ef55c6c5e7d03a4b
ccwow123 <1849325557@qq.com>
2023-04-01 10:01:05 +0800

    v0.1 成功运行

--- train_dev.py
+++ train_dev.py
@@ -6,21 +6,18 @@ import torch
 
 from src import UNet
 from train_utils import train_one_epoch, evaluate, create_lr_scheduler
-from utils.my_dataset import VOCSegmentation
+from utils.my_dataset import DriveDataset
 from utils import transforms as T
 
 
 class SegmentationPresetTrain:
-    def __init__(self, base_size, crop_size, hflip_prob=0.5, vflip_prob=0.5,
-                 mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)):
+    def __init__(self, base_size, crop_size, hflip_prob=0.5, mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)):
         min_size = int(0.5 * base_size)
-        max_size = int(1.2 * base_size)
+        max_size = int(2.0 * base_size)
 
         trans = [T.RandomResize(min_size, max_size)]
         if hflip_prob > 0:
             trans.append(T.RandomHorizontalFlip(hflip_prob))
-        if vflip_prob > 0:
-            trans.append(T.RandomVerticalFlip(vflip_prob))
         trans.extend([
             T.RandomCrop(crop_size),
             T.ToTensor(),
@@ -33,8 +30,9 @@ class SegmentationPresetTrain:
 
 
 class SegmentationPresetEval:
-    def __init__(self, mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)):
+    def __init__(self, base_size, mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)):
         self.transforms = T.Compose([
+            T.RandomResize(base_size, base_size),
             T.ToTensor(),
             T.Normalize(mean=mean, std=std),
         ])
@@ -43,19 +41,34 @@ class SegmentationPresetEval:
         return self.transforms(img, target)
 
 
-def get_transform(train, mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)):
-    base_size = 64
-    crop_size = 60
+def get_transform(train):
+    base_size = 520
+    crop_size = 480
 
-    if train:
-        return SegmentationPresetTrain(base_size, crop_size, mean=mean, std=std)
-    else:
-        return SegmentationPresetEval(mean=mean, std=std)
-
+    return SegmentationPresetTrain(base_size, crop_size) if train else SegmentationPresetEval(base_size)
 
 def create_model(num_classes):
     model = UNet(in_channels=3, num_classes=num_classes, base_c=32)
     return model
+# def create_model(num_classes, pretrain=False):
+#     model = lraspp_mobilenetv3_large(num_classes=num_classes)
+#
+#     if pretrain:
+#         weights_dict = torch.load("./lraspp_mobilenet_v3_large.pth", map_location='cpu')
+#
+#         if num_classes != 21:
+#             # 官方提供的预训练权重是21类(包括背景)
+#             # 如果训练自己的数据集，将和类别相关的权重删除，防止权重shape不一致报错
+#             for k in list(weights_dict.keys()):
+#                 if "low_classifier" in k or "high_classifier" in k:
+#                     del weights_dict[k]
+#
+#         missing_keys, unexpected_keys = model.load_state_dict(weights_dict, strict=False)
+#         if len(missing_keys) != 0 or len(unexpected_keys) != 0:
+#             print("missing_keys: ", missing_keys)
+#             print("unexpected_keys: ", unexpected_keys)
+#
+#     return model
 
 
 def main(args):
@@ -64,13 +77,10 @@ def main(args):
     # segmentation nun_classes + background
     num_classes = args.num_classes + 1
 
-    # using compute_mean_std.py
-    mean = (0.709, 0.381, 0.224)
-    std = (0.127, 0.079, 0.043)
-
     # 用来保存训练以及验证过程中信息
     results_file = "results{}.txt".format(datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
 
+    # VOCdevkit -> VOC2012 -> ImageSets -> Segmentation -> train.txt
     train_dataset = VOCSegmentation(args.data_path,
                                     year="2007",
                                     transforms=get_transform(train=True),
@@ -99,7 +109,10 @@ def main(args):
     model = create_model(num_classes=num_classes)
     model.to(device)
 
-    params_to_optimize = [p for p in model.parameters() if p.requires_grad]
+    params_to_optimize = [
+        {"params": [p for p in model.backbone.parameters() if p.requires_grad]},
+        {"params": [p for p in model.classifier.parameters() if p.requires_grad]}
+    ]
 
     optimizer = torch.optim.SGD(
         params_to_optimize,
@@ -120,30 +133,21 @@ def main(args):
         if args.amp:
             scaler.load_state_dict(checkpoint["scaler"])
 
-    best_dice = 0.
     start_time = time.time()
     for epoch in range(args.start_epoch, args.epochs):
-        mean_loss, lr = train_one_epoch(model, optimizer, train_loader, device, epoch, num_classes,
+        mean_loss, lr = train_one_epoch(model, optimizer, train_loader, device, epoch,
                                         lr_scheduler=lr_scheduler, print_freq=args.print_freq, scaler=scaler)
 
-        confmat, dice = evaluate(model, val_loader, device=device, num_classes=num_classes)
+        confmat = evaluate(model, val_loader, device=device, num_classes=num_classes)
         val_info = str(confmat)
         print(val_info)
-        print(f"dice coefficient: {dice:.3f}")
         # write into txt
         with open(results_file, "a") as f:
             # 记录每个epoch对应的train_loss、lr以及验证集各指标
             train_info = f"[epoch: {epoch}]\n" \
                          f"train_loss: {mean_loss:.4f}\n" \
-                         f"lr: {lr:.6f}\n" \
-                         f"dice coefficient: {dice:.3f}\n"
+                         f"lr: {lr:.6f}\n"
             f.write(train_info + val_info + "\n\n")
-
-        if args.save_best is True:
-            if best_dice < dice:
-                best_dice = dice
-            else:
-                continue
 
         save_file = {"model": model.state_dict(),
                      "optimizer": optimizer.state_dict(),
@@ -152,11 +156,7 @@ def main(args):
                      "args": args}
         if args.amp:
             save_file["scaler"] = scaler.state_dict()
-
-        if args.save_best is True:
-            torch.save(save_file, "save_weights/best_model.pth")
-        else:
-            torch.save(save_file, "save_weights/model_{}.pth".format(epoch))
+        torch.save(save_file, "save_weights/model_{}.pth".format(epoch))
 
     total_time = time.time() - start_time
     total_time_str = str(datetime.timedelta(seconds=int(total_time)))
@@ -165,27 +165,25 @@ def main(args):
 
 def parse_args():
     import argparse
-    parser = argparse.ArgumentParser(description="pytorch unet training")
+    parser = argparse.ArgumentParser(description="pytorch lraspp training")
 
-    parser.add_argument("--data-path", default=r"D:\Files\_datasets\Dataset-reference\VOCdevkit_cap_c5_bin", help="DRIVE root")
-    # exclude background
-    parser.add_argument("--num-classes", default=1, type=int)
+    parser.add_argument("--data-path", default=r"D:\Files\_datasets\Dataset-reference\VOCdevkit_End_skew", help="VOCdevkit root")
+    parser.add_argument("--num-classes", default=2, type=int)
     parser.add_argument("--device", default="cuda", help="training device")
-    parser.add_argument("-b", "--batch-size", default=1, type=int)
-    parser.add_argument("--epochs", default=2, type=int, metavar="N",
+    parser.add_argument("-b", "--batch-size", default=4, type=int)
+    parser.add_argument("--epochs", default=30, type=int, metavar="N",
                         help="number of total epochs to train")
 
-    parser.add_argument('--lr', default=0.01, type=float, help='initial learning rate')
+    parser.add_argument('--lr', default=0.0001, type=float, help='initial learning rate')
     parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
                         help='momentum')
     parser.add_argument('--wd', '--weight-decay', default=1e-4, type=float,
                         metavar='W', help='weight decay (default: 1e-4)',
                         dest='weight_decay')
-    parser.add_argument('--print-freq', default=20, type=int, help='print frequency')
+    parser.add_argument('--print-freq', default=10, type=int, help='print frequency')
     parser.add_argument('--resume', default='', help='resume from checkpoint')
     parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
                         help='start epoch')
-    parser.add_argument('--save-best', default=True, type=bool, help='only save best dice weights')
     # Mixed precision training parameters
     parser.add_argument("--amp", default=False, type=bool,
                         help="Use torch.cuda.amp for mixed precision training")
