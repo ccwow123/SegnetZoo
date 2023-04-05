87ea19449c61bbdbb9d44ebd8ae158b8c242b331
ccwow123 <1849325557@qq.com>
2023-04-05 16:42:52 +0800

    v0.39 加入predict.py

--- predict.py
+++ predict.py
@@ -1,77 +1,78 @@
+import glob
 import os
-import time
+import sys
+sys.path.insert(0, '../')
 
+import time
+import json
+import argparse
+import cv2
 import torch
 from torchvision import transforms
 import numpy as np
 from PIL import Image
+from collections import namedtuple
+from itertools import product
+from tqdm import tqdm, trange
+import re
+from utils import transforms as T
+from utils.my_dataset import VOCSegmentation
+# 调试板初始化
+def palette_init():
+    palette_path = r'palette_utils/palette.json'
+    assert os.path.exists(palette_path), f"palette {palette_path} not found."
+    with open(palette_path, "rb") as f:
+        pallette_dict = json.load(f)
+        pallette = []
+        for v in pallette_dict.values():
+            pallette += v
+    return pallette
+# 创建输出路径文件夹
+def create_save_path(weights_path):
 
-from src import UNet
+    model_name = weights_path.split('/')[-2].split('-')[-1]
+    save_path = os.path.join('output', model_name)
 
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+    return save_path
 
-def time_synchronized():
-    torch.cuda.synchronize() if torch.cuda.is_available() else None
-    return time.time()
+def get_transform(base_size, mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)):
+    class SegmentationPresetTest:
+        def __init__(self, base_size, mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)):
+            self.transforms = T.Compose([
+                T.Resize(base_size),
+                T.ToTensor(),
+                T.Normalize(mean=mean, std=std),
+            ])
 
-
-def main():
-    classes = 1  # exclude background
-    weights_path = "./save_weights/best_model.pth"
-    img_path = "./DRIVE/test/images/01_test.tif"
-    roi_mask_path = "./DRIVE/test/mask/01_test_mask.gif"
-    assert os.path.exists(weights_path), f"weights {weights_path} not found."
-    assert os.path.exists(img_path), f"image {img_path} not found."
-    assert os.path.exists(roi_mask_path), f"image {roi_mask_path} not found."
-
-    mean = (0.709, 0.381, 0.224)
-    std = (0.127, 0.079, 0.043)
-
-    # get devices
+        def __call__(self, img, target):
+            return self.transforms(img, target)
+    return SegmentationPresetTest(base_size, mean, std)
+def main(args):
+    # 初始化
+    palette = palette_init()
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    print("using {} device.".format(device))
+    save_path=create_save_path(args.weights_path)
+    pre_dataset= VOCSegmentation(args.data_path,
+                                  year="2007",
+                                  transforms=get_transform(args.img_size),
+                                  txt_name="test.txt")
 
-    # create model
-    model = UNet(in_channels=3, num_classes=classes+1, base_c=32)
+def parse_args():
+    parser = argparse.ArgumentParser(description="pytorch segnets training")
+    # 主要
+    parser.add_argument("--weights_path", default=r'logs/04-05_16-12-15-Unet_c2f/best_model.pth', type=str, help="权重路径")
+    parser.add_argument("--data_path", default=r'D:\Files\_datasets\Dataset-reference\VOC_E_skew', help="VOCdevkit 路径")
+    parser.add_argument("--num-classes", default=2, type=int,help="分类总数")
+    parser.add_argument("--img-size", default=512, type=int,help="图片缩放大小")
+    parser.add_argument("--method", default="fusion",  choices=["fusion", "mask", "contours"], help="输出方式")
+    # 其他
+    parser.add_argument("--label", default="End skew", type=str, help="contours方式下的标签")
 
-    # load weights
-    model.load_state_dict(torch.load(weights_path, map_location='cpu')['model'])
-    model.to(device)
+    args = parser.parse_args()
 
-    # load roi mask
-    roi_img = Image.open(roi_mask_path).convert('L')
-    roi_img = np.array(roi_img)
-
-    # load image
-    original_img = Image.open(img_path).convert('RGB')
-
-    # from pil image to tensor and normalize
-    data_transform = transforms.Compose([transforms.ToTensor(),
-                                         transforms.Normalize(mean=mean, std=std)])
-    img = data_transform(original_img)
-    # expand batch dimension
-    img = torch.unsqueeze(img, dim=0)
-
-    model.eval()  # 进入验证模式
-    with torch.no_grad():
-        # init model
-        img_height, img_width = img.shape[-2:]
-        init_img = torch.zeros((1, 3, img_height, img_width), device=device)
-        model(init_img)
-
-        t_start = time_synchronized()
-        output = model(img.to(device))
-        t_end = time_synchronized()
-        print("inference time: {}".format(t_end - t_start))
-
-        prediction = output['out'].argmax(1).squeeze(0)
-        prediction = prediction.to("cpu").numpy().astype(np.uint8)
-        # 将前景对应的像素值改成255(白色)
-        prediction[prediction == 1] = 255
-        # 将不敢兴趣的区域像素设置成0(黑色)
-        prediction[roi_img == 0] = 0
-        mask = Image.fromarray(prediction)
-        mask.save("test_result.png")
-
-
+    return args
 if __name__ == '__main__':
-    main()
+    args = parse_args()
+    main(args)
