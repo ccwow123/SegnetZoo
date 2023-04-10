23290f1053a7981c9d9629046a9385f382d69648
ccwow123 <1849325557@qq.com>
2023-04-10 22:01:34 +0800

    v0.64 Unet0c3_v2_3不使用SPPF，加入注意力

--- src/unet_mod/unet0c3.py
+++ src/unet_mod/unet0c3.py
@@ -215,6 +215,50 @@ class Unet0c3_v2_2(nn.Module):
         x = self.up4(x, self.att_1(x1))
         logits = self.out_conv(x)
         return logits
+# v2_3不使用SPPF，加入注意力
+class Unet0c3_v2_3(nn.Module):
+    def __init__(self,
+                 in_channels: int = 3,
+                 num_classes: int = 2,
+                 bilinear: bool = False,
+                 base_c: int = 32,
+                 attention='ca'):
+        super().__init__()
+        self.in_channels = in_channels
+        self.num_classes = num_classes
+        self.bilinear = bilinear
+
+        self.in_conv = Conv(in_channels, base_c)
+        self.down1 = Down(base_c, base_c * 2)
+        self.down2 = Down(base_c * 2, base_c * 4)
+        self.down3 = Down(base_c * 4, base_c * 8)
+        factor = 2 if bilinear else 1
+        self.down4 = Down(base_c * 8, base_c * 16 // factor)
+        self.up1 = Up(base_c * 16, base_c * 8 // factor, bilinear)
+        self.up2 = Up(base_c * 8, base_c * 4 // factor, bilinear)
+        self.up3 = Up(base_c * 4, base_c * 2 // factor, bilinear)
+        self.up4 = Up(base_c * 2, base_c, bilinear)
+        self.out_conv = OutConv(base_c, num_classes)
+
+        if attention == 'ca':
+            self.att_1 = CoordAtt(base_c * 1)
+            self.att_2 = CoordAtt(base_c * 2)
+            self.att_3 = CoordAtt(base_c * 4)
+            self.att_4 = CoordAtt(base_c * 8)
+
+    def forward(self, x: torch.Tensor) :
+        x1 = self.in_conv(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        x = self.up1(x5, self.att_4(x4))
+        x = self.up2(x, self.att_3(x3))
+        x = self.up3(x, self.att_2(x2))
+        x = self.up4(x, self.att_1(x1))
+        logits = self.out_conv(x)
+        return logits
+
 if __name__ == '__main__':
-    model = Unet0c3_v2_2(3,2,attention='ca')
+    model = Unet0c3_v2_3(3,2,attention='ca')
     model_test(model,(2,3,256,256),'shape')
