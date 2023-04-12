65d8a33af903110e974eb65211646d778f54107a
ccwow123 <1849325557@qq.com>
2023-04-12 10:39:53 +0800

    v0.69 加入Unet0c3_v4

--- src/unet_mod/unet0c3.py
+++ src/unet_mod/unet0c3.py
@@ -403,7 +403,79 @@ class Unet0c3_v3_1(nn.Module):
         logits = self.out_conv(x)
 
         return logits
+
+# v4 使用C3 n=2
+
+class Up_v4(nn.Module):
+    def __init__(self, in_channels, out_channels, bilinear=True):
+        super().__init__()
+        if bilinear:
+            self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+            self.conv = C3(in_channels, out_channels,n=2)
+        else:
+            self.up = nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size=2, stride=2)
+            self.conv = C3(in_channels, out_channels,n=2)
+
+    def forward(self, x1: torch.Tensor, x2: torch.Tensor) -> torch.Tensor:
+        x1 = self.up(x1)
+        # # [N, C, H, W]
+        # diff_y = x2.size()[2] - x1.size()[2]
+        # diff_x = x2.size()[3] - x1.size()[3]
+        #
+        # # padding_left, padding_right, padding_top, padding_bottom
+        # x1 = F.pad(x1, [diff_x // 2, diff_x - diff_x // 2,
+        #                 diff_y // 2, diff_y - diff_y // 2])
+
+        x = torch.cat([x2, x1], dim=1)
+        x = self.conv(x)
+        return x
+
+class Down_v4(nn.Sequential):
+    def __init__(self, in_channels, out_channels):
+        super().__init__(
+            Conv(in_channels, out_channels, 3, 2, 1),
+            C3(out_channels, out_channels,n=2)
+        )
+
+class Unet0c3_v4(nn.Module):
+    def __init__(self,
+                 in_channels: int = 3,
+                 num_classes: int = 2,
+                 bilinear: bool = False,
+                 base_c: int = 32):
+        super().__init__()
+        self.in_channels = in_channels
+        self.num_classes = num_classes
+        self.bilinear = bilinear
+
+        self.in_conv = Conv(in_channels, base_c)
+        self.down1 = Down_v4(base_c, base_c * 2)
+        self.down2 = Down_v4(base_c * 2, base_c * 4)
+        self.down3 = Down_v4(base_c * 4, base_c * 8)
+        factor = 2 if bilinear else 1
+        self.down4 = Down_v4(base_c * 8, base_c * 16 // factor)
+        self.up1 = Up_v4(base_c * 16, base_c * 8 // factor, bilinear)
+        self.up2 = Up_v4(base_c * 8, base_c * 4 // factor, bilinear)
+        self.up3 = Up_v4(base_c * 4, base_c * 2 // factor, bilinear)
+        self.up4 = Up_v4(base_c * 2, base_c, bilinear)
+        self.out_conv = OutConv(base_c, num_classes)
+
+    def forward(self, x: torch.Tensor) -> Dict[str, torch.Tensor]:
+        x1 = self.in_conv(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        x = self.up1(x5, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
+        logits = self.out_conv(x)
+
+        return logits
+
 if __name__ == '__main__':
     # model = Unet0c3_v2_4(3,2,attention='S2')
-    model = Unet0c3_v3_1(3,2)
+    # model = Unet0c3_v3_1(3,2)
+    model = Unet0c3_v4(3,2)
     model_test(model,(2,3,256,256),'shape')
--- train.py
+++ train.py
@@ -342,6 +342,8 @@ def create_model(args,in_channels, num_classes, base_c=32):
         model = Unet0c3_v3(in_channels=in_channels, num_classes=num_classes, base_c=base_c)
     elif args.model_name == "Unet0c3_v3_1":
         model = Unet0c3_v3_1(in_channels=in_channels, num_classes=num_classes, base_c=base_c)
+    elif args.model_name == "Unet0c3_v4":
+        model = Unet0c3_v4(in_channels=in_channels, num_classes=num_classes, base_c=base_c)
     else:
         raise ValueError("wrong model name")
     return initialize_weights(model)
